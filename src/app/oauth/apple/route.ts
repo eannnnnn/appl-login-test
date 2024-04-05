0d618833ac3f917518694ed0017a30043e02326c
chongmyeoooooong <cmp2309@kico.co.kr>
2024-04-05 10:52:10 +0900

    test

--- src/app/oauth/apple/route.ts
+++ src/app/oauth/apple/route.ts
@@ -2,6 +2,7 @@ import { redirect } from "next/navigation";
 import { NextRequest } from "next/server";
 
 export async function POST(req: NextRequest) {
+  
   const json = await req.json();
   console.info(json);
   redirect(`/oauth/apple/callback?body=${JSON.stringify(json)}`);
