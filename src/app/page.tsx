f489b931e6bae587634a29d418ba406b5e928c3e
chongmyeoooooong <cmp2309@kico.co.kr>
2024-04-05 10:34:24 +0900

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     const searchParam = new URLSearchParams(location.search);
 
     const redirectUri = `https://${location.hostname}`;
-    const url = `https://appleid.apple.com/auth/authorize?response_type=code&scope=name%20email&client_id=${searchParam.get(
+    const url = `https://appleid.apple.com/auth/authorize?response_type=form_post&scope=name%20email&client_id=${searchParam.get(
       "c"
     )}&redirect_uri=${encodeURI(`${redirectUri}/oauth/apple`)}`;
     location.href = url;
