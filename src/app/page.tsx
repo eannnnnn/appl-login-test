ae8ec2e0d8756942010a345388b810bd7e2ceb29
chongmyeoooooong <cmp2309@kico.co.kr>
2024-04-05 10:10:47 +0900

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
 "use client";
 
-import { useSearchParams } from "next/navigation";
-
 export default function Home() {
-  const searchParam = useSearchParams();
   const onAppleClick = () => {
+    const searchParam = new URLSearchParams(location.href);
+
     const redirectUri = `https://${location.hostname}`;
     const url = `https://appleid.apple.com/auth/authorize?response_type=code&client_id=${searchParam.get(
       "c"
     )}&redirect_uri=${encodeURI(`${redirectUri}/oauth/apple`)}&state=SIGN_UP`;
     location.href = url;
   };
+
   return (
     <button onClick={onAppleClick} className="btn bg-white">
       APPLE 회원가입
