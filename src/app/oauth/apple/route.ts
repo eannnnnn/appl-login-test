114e5e8a1faa46f46d3040ba16492cf011e4fc39
chongmyeoooooong <cmp2309@kico.co.kr>
2024-04-05 10:57:50 +0900

    test

--- src/app/oauth/apple/route.ts
+++ src/app/oauth/apple/route.ts
@@ -2,8 +2,9 @@ import { redirect } from "next/navigation";
 import { NextRequest } from "next/server";
 
 export async function POST(req: NextRequest) {
-  
-  const json = await req.json();
-  console.info(json);
-  redirect(`/oauth/apple/callback?body=${JSON.stringify(json)}`);
+  const form = await req.formData();
+  console.info(form);
+  // const json = await req.json();
+  // console.info(json);
+  redirect(`/oauth/apple/callback?body=${JSON.stringify(form)}`);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     const searchParam = new URLSearchParams(location.search);
 
     const redirectUri = `https://${location.hostname}`;
-    const url = `https://appleid.apple.com/auth/authorize?response_type=code%20id_token&client_id=${searchParam.get(
+    const url = `https://appleid.apple.com/auth/authorize?response_mode=form_post&scope=name%20email&response_type=code&client_id=${searchParam.get(
       "c"
     )}&redirect_uri=${encodeURI(`${redirectUri}/oauth/apple`)}`;
     location.href = url;
