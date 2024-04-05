197c6de2230d41b54ab3d978d3180e014bf20ff8
chongmyeoooooong <cmp2309@kico.co.kr>
2024-04-05 11:23:16 +0900

    test

--- src/app/oauth/apple/route.ts
+++ src/app/oauth/apple/route.ts
@@ -7,6 +7,8 @@ export async function POST(req: NextRequest) {
   // const json = await req.json();
   // console.info(json);
   redirect(
-    `/oauth/apple/callback?body=${JSON.stringify(Array.from(form.entries()))}`
+    `/oauth/apple/callback?body=${encodeURI(
+      JSON.stringify(Array.from(form.entries()))
+    )}`
   );
 }
